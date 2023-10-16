a9daaf2d5f92b3d42b3f87995ad01f7cc554c7bb
tunalad <novica.arsic@aol.com>
2023-10-16 21:51:45 +0200

    added an optional config feature
    in the config file we can define our paths, and set options from there.
    Passing options when calling neft, only the options set to false will be
    overwriten

--- main.py
+++ main.py
@@ -2,21 +2,34 @@
 # pylint: disable=consider-using-sys-exit
 
 # NeFT - New From Template
-# Create a file from pre-defined templates.
+# Create files from pre-defined templates.
 
 import argparse
 from shutil import copy
 import os
 import sys
-from utils import rlinput, find_templates, devicon_handler, generate_menu
+from utils import rlinput, find_templates, devicon_handler, generate_menu, load_config
+
 
 def main(args):
     # FINDING TEMPLATES
-    paths = ["~/.local/share/templates/"]
-    TEMPLATE_FILES = find_templates(paths=paths, use_xdg_path=args.get("use_xdg_path", False))
+    paths = config.get("paths", ["~/Templates"])
+    try:
+        TEMPLATE_FILES = find_templates(
+            paths=paths, use_xdg_path=args.get("use_xdg_path", False)
+        )
+    except:
+        sys.exit(
+            "[NeFT] Couldn't find any template directories. Either create ~/Templates or define paths in neft.yaml."
+        )
 
     # INPUT HANDLING
-    menu_entry_index = generate_menu(TEMPLATE_FILES, icons=args.get("icons", False), full_path=args.get("full_path", False), output=args.get('output', None))
+    menu_entry_index = generate_menu(
+        TEMPLATE_FILES,
+        icons=args.get("icons", False),
+        full_path=args.get("full_path", False),
+        output=args.get("output", None),
+    )
 
     if menu_entry_index == None or menu_entry_index == 0:
         sys.exit()
@@ -24,11 +37,13 @@ def main(args):
         SELECTED = menu_entry_index - 1
 
     # FILE CREATION
-    newName = ''
-    if args.get('output'): 
-        newName = args.get('output')
+    newName = ""
+    if args.get("output"):
+        newName = args.get("output")
     else:
-        newName = rlinput(f'[NeFT] Create file: ', os.path.basename(TEMPLATE_FILES[SELECTED]))
+        newName = rlinput(
+            f"[NeFT] Create file: ", os.path.basename(TEMPLATE_FILES[SELECTED])
+        )
 
     if len(newName.strip()) > 0:
         # copying file
@@ -37,16 +52,38 @@ def main(args):
     else:
         print("[NeFT] Invalid file name. Aborting.")
 
-    return args.get('loop', False)
+    return args.get("loop", False)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='NeFT - New From Template - Create a file from pre-defined templates.')
-    parser.add_argument('-i', '--icons', action='store_true', help='draw icons in the menu')
-    parser.add_argument('-l', '--loop', action='store_true', help='enable loop mode')
-    parser.add_argument('-f', '--full-path', action='store_true', help='draw the whole file path')
-    parser.add_argument('-o', '--output', metavar="PATH", type=str, help='output path')
-    parser.add_argument('-xdg', '--use-xdg-path', action='store_true', help='include xdg-user-dir templates path')
-    # Add more arguments as needed
+    # fmt: off
+    parser = argparse.ArgumentParser(description="NeFT - New From Template - Create files from pre-defined templates.")
+    parser.add_argument("-i", "--icons", action="store_true", help="draw icons in the menu")
+    parser.add_argument("-l", "--loop", action="store_true", help="enable loop mode")
+    parser.add_argument("-f", "--full-path", action="store_true", help="draw the whole file path")
+    parser.add_argument("-xdg", "--use-xdg-path", action="store_true", help="include xdg-user-dir templates path",)
+    parser.add_argument("-c", "--config", metavar="PATH", type=str, help="configuration file path")
+    parser.add_argument("-o", "--output", metavar="PATH", type=str, help="output path")
+    # fmt: on
+
+    # add more arguments as needed
     args = parser.parse_args()
 
-    while main(vars(args)): pass
+    # config overwriting
+    config_path = (
+        args.config
+        or "$XDG_CONFIG_HOME/neft/neft.yaml"
+        or "$XDG_CONFIG_HOME/neft.yaml"
+        or "~/.config/neft/neft.yaml"
+        or "~/.config/neft.yaml"
+        or "~/neft.yaml"
+    )
+
+    config = load_config(config_path)
+    if config:
+        for key, value in config.items():
+            if hasattr(args, key) and value is True:
+                setattr(args, key, value)
+
+    while main(vars(args)):
+        pass
--- utils.py
+++ utils.py
@@ -2,13 +2,16 @@ import os
 import subprocess
 import readline
 import devicons
+import yaml
 from simple_term_menu import TerminalMenu
+
 
 def rlinput(prompt, default_value=""):
     readline.set_startup_hook(lambda: readline.insert_text(default_value))
     user_input = input(f"{prompt}")
     readline.set_startup_hook()
     return user_input
+
 
 def normalize_path(path):
     if path.startswith("~"):
@@ -18,12 +21,15 @@ def normalize_path(path):
     else:
         return path
 
+
 def find_templates(paths=[], use_xdg_path=False):
     if use_xdg_path:
         try:
-            completed_process = subprocess.run(['xdg-user-dir', 'TEMPLATES'], capture_output=True, text=True)
+            completed_process = subprocess.run(
+                ["xdg-user-dir", "TEMPLATES"], capture_output=True, text=True
+            )
             if completed_process.returncode == 0:
-                paths.insert(0, completed_process.stdout.strip() + '/')
+                paths.insert(0, completed_process.stdout.strip() + "/")
         except FileNotFoundError:
             pass
 
@@ -36,12 +42,14 @@ def find_templates(paths=[], use_xdg_path=False):
 
     return templates
 
+
 def devicon_handler(file_name):
     try:
         extension = os.path.splitext(file_name)[-1][1:]
         return devicons.file_node_extensions[extension]
     except KeyError:
         return "?"
+
 
 def generate_menu(files, icons=False, full_path=False, output=None):
     # generates items
@@ -64,3 +72,12 @@ def generate_menu(files, icons=False, full_path=False, output=None):
 
     # returns the menu
     return TerminalMenu(**options).show()
+
+
+def load_config(path):
+    if os.path.exists(path):
+        with open(path, "r") as file:
+            config = yaml.safe_load(file)
+            return config
+    else:
+        return {}
